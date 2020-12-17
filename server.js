913665e4d6dcd6986af3f3dd02fc4e8412273abc
amiedawn <amie.d.carroll@gmail.com>
2020-12-17 12:52:43 -0700

    add prompts

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const inquirer = require("inquirer");
-const cTable = require("console.table");
+const { prompt } = require("inquirer");
 
 // instantiate the server
 const app = express();
@@ -16,6 +15,71 @@ const connection = mysql.createConnection({
   user: "root",
   database: "test",
 });
+
+async function showPrompts() {
+  const { choice } = await prompt ([
+    {
+      type: "list",
+      name: "choice",
+      message: "Choose the task you want to do.",
+      choices: [
+        {
+          name: "View all employees",
+          value: "VIEW_EMPLOYEES"
+        },
+        {
+          name: "View all departments",
+          value: "VIEW_DEPARTMENTS"
+        },
+        {
+          name: "View all roles",
+          value: "VIEW_ROLES"
+        },
+        {
+          name: "Add an employee",
+          value: "ADD_EMPLOYEE"
+        },
+        {
+          name: "Add a department",
+          value: "ADD_DEPARTMENT"
+        },
+        {
+          name: "Add a role",
+          value: "ADD_ROLE"
+        },
+        {
+          name: "Update employee role",
+          value: "UPDATE_ROLE"
+        },
+        {
+          name: "Quit",
+          value: "QUIT"
+        },
+      ]
+    }
+  ]);
+  switch (choice) {
+    case "VIEW_EMPLOYEES":
+      return viewEmployees();
+    case "VIEW_DEPARTMENTS":
+      return viewDepartments();
+    case "VIEW_ROLES":
+      return viewRoles();
+    case "ADD_EMPLOYEE":
+      return addEmployee();
+    case "ADD_DEPARTMENT":
+      return addDepartment();
+    case "ADD_ROLE":
+      return addRole();
+    case "UPDATE_ROLE":
+      return updateRole();
+    default:
+      return quit();  
+  }
+
+  
+
+}
 
 // // simple query
 // connection.query(
