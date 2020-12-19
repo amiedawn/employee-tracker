337c4ed6a0347b5e8e4dae5e07813caa8cdc8524
amiedawn <amie.d.carroll@gmail.com>
2020-12-19 14:02:12 -0700

    add department works

--- server.js
+++ server.js
@@ -43,15 +43,39 @@ async function viewAllEmps() {
 // DEPARTMENT QUERY FUNCTIONS
 //************************* */
 
+// view all departments
 async function viewAllDepts() {
   console.log("in view all depts table");
   console.log("");
-
+  
   let query = "SELECT id AS ID, name AS Department FROM department";
-  console.log("in dept table3");
+  console.log("in view dept table");
   const rows = await dbase.query(query);
   console.table(rows);
 };
+
+// get new department name for adding
+async function getAddDeptData() {
+  console.log("in add dept table");
+  return inquirer
+    .prompt([
+      {
+        name: "deptName",
+        type: "input",
+        message: "Please enter the name of the department you would like to add: ",
+      }
+    ])
+};
+  
+async function addDept(deptInfo) {
+  const deptName = deptInfo.deptName;
+  let query = "INSERT INTO department (name) VALUES (?)";
+  let args = [deptName];
+  const rows = await dbase.query(query, args);
+  console.log(`${deptName} is successfully added as a department.`);
+    //  mainChoices();
+};
+
 
 // async function viewAllDepts() {
 //   console.log("in view all depts table");
@@ -69,6 +93,7 @@ async function viewAllDepts() {
 // EMPROLE QUERY FUNCTIONS
 //************************* */
 
+// view all roles
 async function viewAllRoles() {
   console.log("in view all roles table");
   console.log("");
@@ -83,6 +108,7 @@ async function viewAllRoles() {
 // PROMPT FUNCTIONS
 //************************* */
 
+// prompts for main menu
 async function showPrompts() {
   return inquirer
     .prompt([
@@ -91,19 +117,23 @@ async function showPrompts() {
       name: "choice",
       message: "Please choose the task you want to do: ",
       choices: [
+        "VIEWS:",
         "View all employees",
         "View all departments",
         "View all roles",
+        "ADD:",
         "Add an employee",
         "Add a department",
         "Add a role",
-        "Update role",
+        "OTHER:",
+        "Update an employee role",
         "Quit"
       ],
     },
   ]);
 };  
 
+// make main menu choice happen
 async function mainChoices() {
   let quitLoop = false;
   while (!quitLoop) {
@@ -125,26 +155,28 @@ async function mainChoices() {
       case "Add an employee": {
         const newEmp = await getAddEmpData();
         console.log("Add a new employee", newEmp);
-        await addEmpData(newEmp);
+        await addEmp(newEmp);
         break;
       }
       case "Add a department": {
         const newDept = await getAddDeptData();
         console.log("Add a new department", newDept);
-        await addDeptData(newDept);
+        await addDept(newDept);
         break;
       }
       case "Add a role": {
         const newRole = await getAddRoleData();
         console.log("Add a new role", newRole);
-        await addRoleData(newRole);
+        await addRole(newRole);
         break;
       }
-      case "Update role": {
+      case "Update an employee role": {
         const emp = await getUpdtRoleData();
+        console.log("Update an employee role", newRole);
         await updtRoleData(emp);
         break;
       }
+
       case "Quit": {
         quitLoop = true;
         // 0 is successful exit
@@ -157,5 +189,75 @@ async function mainChoices() {
   }
 };
 
+// edit a department
+async function deptPrompt() {
+  return inquirer
+    .prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "Please choose the task you want to do: ",
+      choices: [
+        "Add a department",
+        "Delete a department",
+        "Return to main menu"
+      ],
+    }
+  ])
+};
+
+// make main menu choice happen
+async function editDepartment() {
+  let quitLoop = false;
+  while (!quitLoop) {
+    const prompt = await deptPrompt();
+    switch (prompt.choice) {
+      case "Add a department": {
+        console.log("in switch add dept");
+        await addDept();
+        break;
+      }
+      case "Delete a department": {
+        await viewAllDepts();
+        console.log("in switch delete dept");
+        await deleteDept();
+        break;   
+      }  
+      case "Return to main menu": {
+        quitLoop = true;
+        // 0 is successful exit
+        process.exit(0);
+        return;
+      }
+      default:
+        console.log("Please choose a valid option");
+    }
+  }
+};
+
+//************************* */
+// OTHER FUNCTIONS
+//************************* */
+
+// confirm valid entries
+// async function validateEntry(input) {
+//   console.log('made it to validate input');
+//   if ((input.trim() != '') && (input.trim().length <= 30)) {
+//     return true;
+//   } else {
+//     return "Invalid entry. Please make sure your entry is 30 characters or less."
+//   }
+// };
+
 
 mainChoices();
+
+// add dept function not working - says column name can't be null, though putting in data
+// async function addDept(deptName) {
+//   const newDept = deptName.newDept;
+//   let query = "INSERT INTO department (name) VALUES (?)";
+//   let args = [newDept];
+//   const rows = await dbase.query(query, args);
+//   console.log(`${newDept} is successfully added as a department.`);
+//     //  mainChoices();
+// };
